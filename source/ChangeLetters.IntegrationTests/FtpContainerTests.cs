8c88bd1eb21df81aff681d6c717d7aad53230d78
Robert Härle <rh@bhl-informatik.de>
2025-09-22 17:17:30 +0200

    use ftpClient in each test instead of SetUp

--- source/ChangeLetters.IntegrationTests/FtpContainerTests.cs
+++ source/ChangeLetters.IntegrationTests/FtpContainerTests.cs
@@ -8,13 +8,11 @@ namespace ChangeLetters.IntegrationTests
     //[Ignore("not yet resolved network issues in github actions")]
     public class FtpContainerTests()
     {
-        private IAsyncFtpClient _ftpClient;
         private CancellationTokenSource _cts;
         private ILogger<FtpContainerTests> _log;
         [SetUp]
         public void Setup()
         {
-            _ftpClient = FtpHelpers.GetFtpClient();
             _log = LogHelper.GetLogger<FtpContainerTests>();
             _cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
         }
@@ -23,16 +21,15 @@ namespace ChangeLetters.IntegrationTests
         public async Task TearDown()
         {
             _cts.Dispose();
-            await _ftpClient.Disconnect();
-            _ftpClient.Dispose();
-        }
+         }
 
         [Test]
         public async Task CheckConnection()
         {
+            await using var ftpClient = FtpHelpers.GetFtpClient();
             await Task.Delay(TimeSpan.FromSeconds(2));
             _log.LogInformation($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}] Start test CheckConnection");
-            var profile = await _ftpClient.AutoConnect(_cts.Token);
+            var profile = await ftpClient.AutoConnect(_cts.Token);
 
             Assert.That(profile, Is.Not.Null);
         }
@@ -40,12 +37,13 @@ namespace ChangeLetters.IntegrationTests
         [Test]
         public async Task CopyFile()
         {
+            await using var ftpClient = FtpHelpers.GetFtpClient();
             await Task.Delay(TimeSpan.FromSeconds(2));
-            _log.LogInformation($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}] Start test CopyFile");
+            _log.LogInformation("connect to FTP server {host}/{port} as {user}", ftpClient.Host, ftpClient.Port, ftpClient.Credentials.UserName);
             FtpProfile? profile = null;
             try
             {
-                profile = await _ftpClient.AutoConnect(_cts.Token);
+                profile = await ftpClient.AutoConnect(_cts.Token);
             }
             catch (Exception ex)
             {
@@ -56,8 +54,8 @@ namespace ChangeLetters.IntegrationTests
             {
                 try
                 {
-                    _ftpClient.Config.DataConnectionType = FtpDataConnectionType.EPSV;
-                    profile = await _ftpClient.AutoConnect(_cts.Token);
+                    ftpClient.Config.DataConnectionType = FtpDataConnectionType.EPSV;
+                    profile = await ftpClient.AutoConnect(_cts.Token);
                 }
                 catch (Exception ex)
                 {
@@ -65,10 +63,11 @@ namespace ChangeLetters.IntegrationTests
                 }
                 _log.LogInformation($"EPSV upload connection found {(profile == null ? "no connection" : "connection")}");
             }
-            var d = await _ftpClient.GetListing("/", _cts.Token);
-            var status = await _ftpClient.UploadFile("Files/01 - Der Ölprinz.mp3", "working/01 - Der ?lprinz.mp3", FtpRemoteExists.Overwrite, token:_cts.Token);
-
-            status.ShouldBe(FtpStatus.Success);
+            var d = await ftpClient.GetListing("/", _cts.Token);
+            var result = await ftpClient.UploadFile("Files/01 - Der Ölprinz.mp3", "working/01 - Der ?lprinz.mp3", FtpRemoteExists.Overwrite, token: _cts.Token);
+            await ftpClient.Disconnect();
+            _log.LogInformation("upload resulted in {result}", result);
+            Assert.That(result, Is.EqualTo(FtpStatus.Success));
         }
     }
 }
